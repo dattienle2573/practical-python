5cc665cf8721d8026ec75ab8d42d6b6a01198098
dat <dattienle2573@gmail.com>
2024-04-19 22:15:27 -0400

    update

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,33 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 0
+
+for i in range(12):
+    if principal > 0:
+        principal = principal * (1 + rate / 12) - (payment + extra_payment)
+        total_paid = total_paid + (payment + extra_payment)
+        month += 1
+        print(month, total_paid, principal)
+
+while principal > 0 and principal * (1 + rate / 12) > payment:
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+    month += 1
+    print(month, total_paid, principal)
+
+total_paid = total_paid + principal * (1 + rate / 12)
+principal = 0
+month += 1
+print(month, total_paid, principal)
+
+print("Total paid", total_paid, "month", month)
